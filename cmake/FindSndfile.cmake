5a3a56b714941120b2c1a0ecefbd19a1f51a4bdd
Alexander Grund <Flamefire@users.noreply.github.com>
2020-07-03 12:56:29 +0200

    Fix CMake warning about case mismatch of sndfile

--- cmake/FindSndfile.cmake
+++ cmake/FindSndfile.cmake
@@ -13,6 +13,6 @@ find_library(SNDFILE_LIBRARY NAMES sndfile
     HINTS $ENV{SNDFILE_ROOT}/lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SNDFILE DEFAULT_MSG SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
+find_package_handle_standard_args(Sndfile DEFAULT_MSG SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
 
 mark_as_advanced(SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
