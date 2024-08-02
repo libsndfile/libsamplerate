07b587edf21cd8cf95fee4db9fd11d9c6c1ec4f8
John Hotovy <jghotovy92@gmail.com>
2024-08-02 15:45:44 -0600

    Build prefab package with ./gradlew assembleRelease; or build and run tests on device with ./gradlew ndkTest

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,2 +1,25 @@
+@file:Suppress("UnstableApiUsage")
+
 rootProject.name = "samplerate"
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
